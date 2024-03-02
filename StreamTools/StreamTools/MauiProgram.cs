e73c99f515f94e3d27741f498d1c8050d1e21263
Andrew Diego Beers <andrew.d.beers@gmail.com>
2024-03-02 18:52:46 -0500

    feat: Add in Resource dictionary for colors and provide a consistent style sheet to apply these colors

--- StreamTools/StreamTools/MauiProgram.cs
+++ StreamTools/StreamTools/MauiProgram.cs
@@ -3,9 +3,9 @@ using Blazorise;
 using Blazorise.Icons.Material;
 using Blazorise.Material;
 using Serilog;
-using Serilog.Exceptions;
 using Serilog.Formatting.Compact;
 using Blazorise.Icons.FontAwesome;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace StreamTools;
 public static class MauiProgram
@@ -16,8 +16,6 @@ public static class MauiProgram
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .Enrich.FromLogContext()
-            .Enrich.WithDemystifiedStackTraces()
-            .Enrich.WithExceptionDetails()
             .Enrich.WithThreadName()
             .Enrich.WithThreadId()
             .Enrich.WithProcessId()
@@ -27,7 +25,7 @@ public static class MauiProgram
             .Enrich.WithMemoryUsage()
             .WriteTo.Async(a =>
             {
-                a.Console(new CompactJsonFormatter());
+                a.Console(theme: AnsiConsoleTheme.Code);
                 a.Debug(new CompactJsonFormatter());
             })
             .CreateLogger();
@@ -44,8 +42,7 @@ public static class MauiProgram
             builder.Services.AddMauiBlazorWebView();
             builder.Services.AddBlazorise(options => options.Immediate = true)
                 .AddMaterialProviders()
-                .AddMaterialIcons()
-                .AddFontAwesomeIcons();
+                .AddMaterialIcons();
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Logging.AddDebug();
